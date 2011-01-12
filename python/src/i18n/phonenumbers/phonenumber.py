ff736b7f9a895edbe268ff4231b931926b5c57c7
Teren Teh <tereno@gmail.com>
2011-01-11 22:20:07 -0500

    Ported phonenumber object

---
+++ python/src/i18n/phonenumbers/phonenumber.py
@@ -0,0 +1,13 @@
+# This class is based on Phonenumber.java
+
+class PhoneNumber:
+
+    # Constructor
+    def __init__(self):
+        pass
+
+    # Enum - CountryCodeSource
+    CountryCodeSource = ['FROM_NUMBER_WITH_PLUS_SIGN',
+                         'FROM_NUMBER_WITH_IDD',
+                         'FROM_NUMBER_WITHOUT_PLUS_SIGN',
+                         'FROM_DEFAULT_COUNTRY']
